ff935861e78b31da295d51079e123fc333a32989
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-03-08 21:35:26 -0300

    feat: add validations to CreateItemDto

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,8 +1,28 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateItemDto {
+  @IsNotEmpty()
+  @MinLength(3)
   name: string;
+
+  @MaxLength(250)
   description?: string;
+
+  @IsNumber()
   price: number;
+
+  @IsNotEmpty()
   slug: string;
+
+  @IsBoolean()
   published?: boolean = false;
+
+  @IsNumber()
   categoryId: number;
 }
